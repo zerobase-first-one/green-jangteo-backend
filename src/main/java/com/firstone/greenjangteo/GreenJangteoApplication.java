05df3116dd4c9f45062e0a8a683100a22b8a5945
Sung Hyobin <hellmir610@gmail.com>
2023-11-27 04:22:01 +0900

    Initial commit (#1)

---
+++ src/main/java/com/firstone/greenjangteo/GreenJangteoApplication.java
@@ -0,0 +1,11 @@
+package com.firstone.greenjangteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenJangteoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenJangteoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/firstone/greenjangteo/GreenJangteoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.firstone.greenjangteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource("classpath:application-test.properties")
+@ContextConfiguration(classes = GreenJangteoApplication.class)
+class GreenJangteoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
