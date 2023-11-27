22487d5e7356580c66aa6e4269859b677483b80b
Sung Hyobin <hellmir610@gmail.com>
2023-11-27 22:08:37 +0900

    chore(CI/CD): CI/CD 파이프라인 구축 (#1)
    - Configuration Files
        - Jenkinsfile
        - Dockerfile
    
    - Shell Scripts
        - set-up-docker.sh
        - deploy.sh
        - check-and-restart.sh

---
+++ set-up-docker.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# check if docker is already installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+else
+    echo "Docker is already installed."
+fi
+
+# docker authorization
+sudo usermod -aG docker ubuntu
+
+# start docker service
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# pull MySQL image if not exists
+if [ ! "$(docker images -q mysql:latest)" ]; then
+    echo "Pulling MySQL Docker image..."
+    sudo docker pull mysql:latest
+fi
+
+# create docker network if not exists
+if [ ! "$(docker network ls | grep docker-network)" ]; then
+    sudo docker network create docker-network
+fi
+
+# run MySQL container if not exists
+MYSQL_ROOT_PASSWORD=$1
+DB_USER_NAME=$2
+DB_USER_PASSWORD=$3
+
+if [ ! "$(docker ps -a | grep mysql-container)" ]; then
+    echo "Starting MySQL container..."
+    sudo docker run -d --name mysql-container --network=docker-network \
+    -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+    mysql:latest
+
+    sleep 30
+
+    # create database
+    sudo docker exec mysql-container mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS users;"
+
+    # create user and grant privileges
+    sudo docker exec mysql-container mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD'; GRANT ALL PRIVILEGES ON users.* TO '$DB_USER_NAME'@'%'; FLUSH PRIVILEGES;"
+fi
+
+# pull Redis image if not exists
+if [ ! "$(docker images -q redis:latest)" ]; then
+    echo "Pulling Redis Docker image..."
+    sudo docker pull redis:latest
+fi
+
+# run Redis container if not exists
+REDIS_PASSWORD=$4
+
+if [ ! "$(docker ps -a | grep redis-container)" ]; then
+    echo "Starting Redis container..."
+    sudo docker run -d --name redis-container --network=docker-network \
+    -e REDIS_PASSWORD=$REDIS_PASSWORD \
+    redis:latest
+fi
+
+echo "Docker setup complete."
