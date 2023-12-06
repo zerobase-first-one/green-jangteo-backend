27d135794f77105758d9aeb346026b7718122186
youngvctr <jhk91@yonsei.ac.kr>
2023-12-06 19:18:14 +0900

    fix(cart) : 변경된 ERD 반영, 장바구니 추가/조회/수정  기능 보완 및 삭제 추가

--- src/main/java/com/firstone/greenjangteo/cart/domain/dto/CartProductDto.java
+++ src/main/java/com/firstone/greenjangteo/cart/domain/dto/CartProductDto.java
@@ -1,6 +1,8 @@
 package com.firstone.greenjangteo.cart.domain.dto;
 
 import com.firstone.greenjangteo.cart.domain.model.Cart;
+import com.firstone.greenjangteo.cart.domain.model.CartProduct;
+import com.firstone.greenjangteo.product.domain.dto.ProductDto;
 import com.firstone.greenjangteo.product.domain.model.Product;
 import lombok.*;
 
@@ -12,13 +14,36 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class CartProductDto {
-    private Cart cart; //장바구니 아이디
-
-    private Product product;
-
-    private int quantity;
-
+    private Long productId;
+    private ProductDto product;
+    private int cartProductQuantity;
     private LocalDateTime createdAt;
-
     private LocalDateTime modifiedAt;
+
+
+    public static CartProductDto filteredCartProductOf(CartProduct cartProduct) {
+        return CartProductDto.builder()
+                .productId(cartProduct.getProduct().getId())
+                .cartProductQuantity(cartProduct.getQuantity())
+                .build();
+    }
+
+    public static CartProductDto of(CartProduct cartProduct) {
+        Product product = cartProduct.getProduct();
+        ProductDto productDto = ProductDto.builder()
+                .productId(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .inventory(product.getInventory())
+                .createdAt(product.getCreatedAt())
+                .modifiedAt(product.getModifiedAt())
+                .build();
+
+        return CartProductDto.builder()
+                .product(productDto)
+                .cartProductQuantity(cartProduct.getQuantity())
+                .createdAt(cartProduct.getCreatedAt())
+                .modifiedAt(cartProduct.getModifiedAt())
+                .build();
+    }
 }
