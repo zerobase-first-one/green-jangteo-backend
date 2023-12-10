35e2a63be3330cabbf816cc34b56eb1c36dc3378
Jeonghun Kim <67629272+youngvctr@users.noreply.github.com>
2023-12-10 18:04:39 +0900

    refactor(product): ProductImageService 리팩토링

--- src/main/java/com/firstone/greenjangteo/product/service/ProductImageService.java
+++ src/main/java/com/firstone/greenjangteo/product/service/ProductImageService.java
@@ -1,5 +1,6 @@
 package com.firstone.greenjangteo.product.service;
 
+import com.firstone.greenjangteo.product.domain.dto.ProductImageDto;
 import com.firstone.greenjangteo.product.domain.model.Product;
 import com.firstone.greenjangteo.product.domain.model.ProductImage;
 import com.firstone.greenjangteo.product.repository.ProductImageRepository;
@@ -18,37 +19,35 @@ public class ProductImageService {
     private final ProductImageRepository productImageRepository;
     private final FileService fileService;
 
-    public void saveProductImage(Product product, ProductImage productImage, String productImageUrl, int position, String productImageLocation) throws Exception {
-        //파일 업로드
+    public void saveProductImage(Product product, String productImageUrl, int position, String productImageLocation) throws Exception {
         if (!StringUtils.isEmpty(productImageUrl)) {
             String imageName = fileService.uploadFile(productImageLocation, productImageUrl, productImageUrl.getBytes());
             String imageUrl = "/images/product/" + imageName;
-            //상품 이미지 정보 저장
-            productImage.saveProductImage(product, imageUrl, position);
+            ProductImage productImage = ProductImageDto.toProductImage(product, imageUrl, position);
             productImageRepository.save(productImage);
         }
     }
 
-    public void updateProductImage(Long productId, Product product, List<String> productImageUrlList, String productImageLocation) throws Exception {
-        if (!productImageUrlList.get(0).isEmpty()) {
+    public void updateProductImage(Long productId, Product product, List<ProductImageDto> productImageUrlList, String productImageLocation) throws Exception {
+        if (!productImageUrlList.isEmpty()) {
             List<ProductImage> savedProductImage = productImageRepository.findByProductId(productId);
 
-            //기존 이미지 파일 삭제
             if (!StringUtils.isEmpty(savedProductImage.get(0))) {
                 for (int i = 0; i < savedProductImage.size(); i++) {
-                    String fileName = savedProductImage.get(i).getUrl().split("\\/")[3];
+                    String fileName = savedProductImage.get(i).getUrl().split("/")[3];
                     fileService.deleteFile(productImageLocation, fileName);
                 }
             }
 
             for (int i = 0; i < productImageUrlList.size(); i++) {
-                String imageName = fileService.uploadFile(productImageLocation, productImageUrlList.get(i), productImageUrlList.get(i).getBytes());
+                String imageName = fileService.uploadFile(productImageLocation, productImageUrlList.get(i).getUrl(), productImageUrlList.get(i).getUrl().getBytes());
                 String imageUrl = "/images/product/" + imageName;
-                ProductImage.builder()
+                ProductImage productImage = ProductImage.builder()
                         .product(product)
                         .url(imageUrl)
                         .position(i)
                         .build();
+                productImageRepository.save(productImage);
             }
         }
     }
