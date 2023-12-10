251cf20d0b29d9a975e9e3a0de95179d94f8cc7a
Jeonghun Kim <67629272+youngvctr@users.noreply.github.com>
2023-12-10 18:29:04 +0900

    refactor(category): Category 리팩토링

--- src/main/java/com/firstone/greenjangteo/product/domain/dto/CategoryDetailDto.java
+++ src/main/java/com/firstone/greenjangteo/product/domain/dto/CategoryDetailDto.java
@@ -1,15 +1,16 @@
 package com.firstone.greenjangteo.product.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CategoryDto {
-    private Long productId;
+public class CategoryDetailDto {
+    @JsonProperty("category")
     private String category;
+    @JsonProperty("level")
     private int level;
 }
