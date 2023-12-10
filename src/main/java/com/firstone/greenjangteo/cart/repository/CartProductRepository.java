e0448d25cea165c24d64f3229240e70d6db9c551
Jeonghun Kim <67629272+youngvctr@users.noreply.github.com>
2023-12-10 18:22:49 +0900

    fix(cart): CartProductRepository 데이터 저장 관련 오류 수정

--- src/main/java/com/firstone/greenjangteo/cart/repository/CartProductRepository.java
+++ src/main/java/com/firstone/greenjangteo/cart/repository/CartProductRepository.java
@@ -1,14 +1,14 @@
 package com.firstone.greenjangteo.cart.repository;
 
-import com.firstone.greenjangteo.cart.domain.dto.CartProductDto;
 import com.firstone.greenjangteo.cart.domain.model.CartProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
 
-    CartProduct findCartProductByCartAndProduct(Long cartId, Long productId);
+    Optional<CartProduct> findByCartIdAndProductId(Long cartId, Long productId);
 
     List<CartProduct> findCartProductsByCartId(Long cartId);
 }
