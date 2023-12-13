66642e7dd119a53cba2be320444d045f7d55f6de
Sung Hyobin <hellmir610@gmail.com>
2023-12-13 22:53:35 +0900

    feat(order): 장바구니 주문 요청 기능 프로덕션 코드 추가 (#112)
    - 장바구니 주문 요청
    - 장바구니 주문 요청 비즈니스 로직
    - 장바구니 주문 데이터 저장
    - 201 status code 반환

---
+++ src/main/java/com/firstone/greenjangteo/order/dto/DtoConstant.java
@@ -0,0 +1,6 @@
+package com.firstone.greenjangteo.order.dto;
+
+public class DtoConstant {
+    public static final String SHIPPING_ADDRESS_VALUE = "배송 주소";
+    public static final String BUYER_ID_VALUE = "구매자 ID";
+}
--- src/main/java/com/firstone/greenjangteo/order/service/OrderServiceImpl.java
+++ src/main/java/com/firstone/greenjangteo/order/service/OrderServiceImpl.java
@@ -1,8 +1,13 @@
 package com.firstone.greenjangteo.order.service;
 
+import com.firstone.greenjangteo.cart.domain.dto.response.CartProductListResponseDto;
+import com.firstone.greenjangteo.cart.service.CartService;
+import com.firstone.greenjangteo.order.dto.request.CartOrderRequestDto;
+import com.firstone.greenjangteo.order.dto.request.OrderProductRequestDto;
 import com.firstone.greenjangteo.order.dto.request.OrderRequestDto;
 import com.firstone.greenjangteo.order.model.entity.Order;
 import com.firstone.greenjangteo.order.repository.OrderRepository;
+import com.firstone.greenjangteo.product.domain.model.Product;
 import com.firstone.greenjangteo.product.service.ProductService;
 import com.firstone.greenjangteo.user.domain.store.model.entity.Store;
 import com.firstone.greenjangteo.user.domain.store.service.StoreService;
@@ -11,12 +16,16 @@ import com.firstone.greenjangteo.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final StoreService storeService;
     private final UserService userService;
     private final ProductService productService;
+    private final CartService cartService;
     private final OrderRepository orderRepository;
 
     @Override
@@ -27,5 +36,38 @@ public class OrderServiceImpl implements OrderService {
         order.getOrderProducts().addOrder(order);
 
         return orderRepository.save(order);
+    }
+
+    @Override
+    public Order createOrderFromCart(CartOrderRequestDto cartOrderRequestDto) {
+        Long buyerId = Long.parseLong(cartOrderRequestDto.getBuyerId());
+        User buyer = userService.getUser(buyerId);
+
+        List<CartProductListResponseDto> cartProductListResponseDtos = cartService.getCartList(buyerId);
+        List<OrderProductRequestDto> orderProductRequestDtos = transferCartToOrderDto(cartProductListResponseDtos);
+
+        Product product = productService.getProduct(Long.parseLong(orderProductRequestDtos.get(0).getProductId()));
+        Store store = product.getStore();
+
+        OrderRequestDto orderRequestDto
+                = OrderRequestDto.of(
+                String.valueOf(store.getSellerId()), cartOrderRequestDto.getBuyerId(),
+                orderProductRequestDtos, cartOrderRequestDto.getShippingAddressDto()
+        );
+
+        Order order = Order.from(store, buyer, orderRequestDto, productService);
+        order.getOrderProducts().addOrder(order);
+
+        return orderRepository.save(order);
+    }
+
+    private List<OrderProductRequestDto> transferCartToOrderDto
+            (List<CartProductListResponseDto> cartProductListResponseDtos) {
+        List<OrderProductRequestDto> orderProductRequestDtos = new ArrayList<>();
+        for (CartProductListResponseDto cartProductListResponseDto : cartProductListResponseDtos) {
+            orderProductRequestDtos.add(OrderProductRequestDto.from(cartProductListResponseDto));
+        }
+
+        return orderProductRequestDtos;
     }
 }
