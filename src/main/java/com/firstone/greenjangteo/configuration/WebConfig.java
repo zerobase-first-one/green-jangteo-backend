dd7e7d08c9386351e5aac367d5ea14796c37a521
Sung Hyobin <hellmir610@gmail.com>
2023-12-19 03:34:47 +0900

    chore(CORS): 허용 메서드 형식 변경  (#56)

--- src/main/java/com/firstone/greenjangteo/configuration/WebConfig.java
+++ src/main/java/com/firstone/greenjangteo/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package com.firstone.greenjangteo.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5173",
                         "http://127.0.0.1:8443"
                 )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
