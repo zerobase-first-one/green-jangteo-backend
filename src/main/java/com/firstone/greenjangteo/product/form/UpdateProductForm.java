9af3cec1c303d9d87354c483d42fd8816e6a7884
Jeonghun Kim <67629272+youngvctr@users.noreply.github.com>
2023-12-23 12:08:35 +0900

    fix(category): 카테고리 변수 타입 수정

--- src/main/java/com/firstone/greenjangteo/product/form/UpdateProductForm.java
+++ src/main/java/com/firstone/greenjangteo/product/form/UpdateProductForm.java
@@ -1,7 +1,5 @@
 package com.firstone.greenjangteo.product.form;
 
-import com.firstone.greenjangteo.product.domain.dto.CategoryDetailDto;
-import com.firstone.greenjangteo.product.domain.dto.CategoryDto;
 import com.firstone.greenjangteo.product.domain.dto.ProductImageDto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
@@ -13,14 +11,14 @@ import java.util.List;
 @Builder
 @Getter
 public class UpdateProductForm {
-    @ApiModelProperty(value = "productId", example = "2")
+    @ApiModelProperty(value = "상품 ID", example = "2")
     private Long productId;
 
     @ApiModelProperty(value = "상품명", example = "New 업사이클링 에코백")
     private String productName;
 
-    @ApiModelProperty(value = "카테고리", example = "[{\"category\": \"생활용품\"}, {\"category\": \"New에코백\"}]")
-    private List<CategoryDetailDto> categories;
+    @ApiModelProperty(value = "카테고리", example = "2")
+    private Long categoryId;
 
     @ApiModelProperty(value = "재고", example = "20")
     private int inventory;
