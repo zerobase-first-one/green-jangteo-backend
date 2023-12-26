f72a2a2977775439eaf554be7ecbde370e6a3792
Sung Hyobin <hellmir610@gmail.com>
2023-12-26 12:10:29 +0900

    test(coupon): 쿠폰 그룹 조회 기능 테스트 코드 추가 (#165)
    - 쿠폰 그룹 조회 요청, 200 status code 응답
    - 쿠폰 그룹 조회 비즈니스 로직
    - 쿠폰 데이터 목록을 페이징 처리해 조회

--- src/test/java/com/firstone/greenjangteo/coupon/controller/CouponControllerTest.java
+++ src/test/java/com/firstone/greenjangteo/coupon/controller/CouponControllerTest.java
@@ -2,6 +2,8 @@ package com.firstone.greenjangteo.coupon.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firstone.greenjangteo.coupon.dto.request.IssueCouponsRequestDto;
+import com.firstone.greenjangteo.coupon.model.entity.Coupon;
+import com.firstone.greenjangteo.coupon.model.entity.CouponGroup;
 import com.firstone.greenjangteo.coupon.service.CouponService;
 import com.firstone.greenjangteo.coupon.testutil.CouponTestObjectFactory;
 import com.firstone.greenjangteo.user.security.CustomAuthenticationEntryPoint;
@@ -11,17 +13,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static com.firstone.greenjangteo.coupon.testutil.CouponTestConstant.*;
 import static com.firstone.greenjangteo.web.ApiConstant.ID_EXAMPLE;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
@@ -59,5 +71,32 @@ class CouponControllerTest {
                         .content(objectMapper.writeValueAsString(issueCouponsRequestDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isAccepted());
+    }
+
+    @DisplayName("쿠폰 그룹 ID를 입력해 쿠폰을 페이징 처리한 쿠폰 그룹을 조회할 수 있다.")
+    @Test
+    @WithMockUser(username = ID_EXAMPLE, roles = {"ADMIN"})
+    void getCouponGroup() throws Exception {
+        // given
+        CouponGroup couponGroup
+                = CouponTestObjectFactory.createCouponGroup(
+                COUPON_NAME1, AMOUNT, DESCRIPTION, ISSUE_QUANTITY1, LocalDate.now(), EXPIRATION_PERIOD1
+        );
+
+        List<Coupon> coupons = CouponTestObjectFactory.createCoupons(couponGroup);
+
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<Coupon> couponPage = new PageImpl<>(coupons, pageable, coupons.size());
+
+        when(couponService.getCouponGroup(eq(1L), any(Pageable.class))).thenReturn(couponPage);
+
+        // when, then
+        mockMvc.perform(get("/coupons/{couponGroupId}", ID_EXAMPLE)
+                        .param("paged", "true")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "id,asc"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
