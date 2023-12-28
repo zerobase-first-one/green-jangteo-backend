91eebe31c8e02f1c43f1b88049fba37461a91cc1
youngvctr <jhk91@yonsei.ac.kr>
2023-12-28 10:11:32 +0900

    fix(search): ElasticSearchConfig elasticsearch host 설정

--- src/main/java/com/firstone/greenjangteo/configuration/ElasticSearchConfig.java
+++ src/main/java/com/firstone/greenjangteo/configuration/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.firstone.greenjangteo.configuration;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -11,10 +12,13 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${EC2_IP_FOR_GREEN_JANGTEO}")
+    private String elasticSearchHost;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(elasticSearchHost + ":9200")
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
