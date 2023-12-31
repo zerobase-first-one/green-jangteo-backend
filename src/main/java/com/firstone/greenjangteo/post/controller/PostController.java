e9517f7a67b5b04f67d6fa51b2642cc832564f1e
Sung Hyobin <hellmir610@gmail.com>
2023-12-31 20:54:19 +0900

    feat(post): 전체 게시글 목록 조회 기능 프로덕션 코드 추가 (#209)
    - 전체 게시글 목록 조회 요청
    - 전체 게시글 목록 조회 비즈니스 로직
    - 전체 게시글 목록을 페이징 처리하고 생성 순서 내림차순으로 정렬해 조회
    - 각 게시물의 조회 수 조회 비즈니스 로직
    - 200 status code 반환
    - 페이징 옵션을 별도의 클래스 상수로 분리
    - FormatConverter 클래스 추가
        - 정렬 방식 정보를 파싱하는 메서드를 별도의 클래스로 분리

--- src/main/java/com/firstone/greenjangteo/post/controller/PostController.java
+++ src/main/java/com/firstone/greenjangteo/post/controller/PostController.java
@@ -4,13 +4,17 @@ import com.firstone.greenjangteo.post.domain.view.model.entity.View;
 import com.firstone.greenjangteo.post.domain.view.model.service.ViewService;
 import com.firstone.greenjangteo.post.dto.PostRequestDto;
 import com.firstone.greenjangteo.post.dto.PostResponseDto;
+import com.firstone.greenjangteo.post.dto.PostsResponseDto;
 import com.firstone.greenjangteo.post.model.entity.Post;
 import com.firstone.greenjangteo.post.service.PostService;
+import com.firstone.greenjangteo.utility.FormatConverter;
 import com.firstone.greenjangteo.utility.InputFormatValidator;
 import com.firstone.greenjangteo.utility.RoleValidator;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +23,10 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
+import static com.firstone.greenjangteo.utility.PagingConstant.*;
 import static com.firstone.greenjangteo.web.ApiConstant.ID_EXAMPLE;
 
 @RestController
@@ -33,10 +40,14 @@ public class PostController {
     private static final String CREATE_POST_DESCRIPTION = "회원 ID와 게시물 내용을 입력해 게시물을 등록할 수 있습니다.";
     private static final String CREATE_POST_FORM = "게시물 등록 양식";
 
+    private static final String GET_ALL_POSTS = "게시글 목록 조회";
+    private static final String GET_ALL_POSTS_DESCRIPTION = "전체 게시글 목록을 조회할 수 있습니다." +
+            "\n페이징 옵션을 선택할 수 있습니다.";
+
     private static final String GET_POST = "게시물 조회";
     private static final String GET_POST_DESCRIPTION = "게시물 ID와 게시자 ID를 입력해 게시물을 조회할 수 있습니다.";
-    private static final String WRITER_ID = "게시자 ID";
     private static final String POST_ID = "게시물 ID";
+    private static final String WRITER_ID = "게시자 ID";
 
     @ApiOperation(value = CREATE_POST, notes = CREATE_POST_DESCRIPTION)
     @PostMapping
@@ -49,6 +60,50 @@ public class PostController {
 
         return buildResponse(PostResponseDto.from(post));
     }
+
+    @ApiOperation(value = GET_ALL_POSTS, notes = GET_ALL_POSTS_DESCRIPTION)
+    @GetMapping()
+    public ResponseEntity<List<PostsResponseDto>> getAllPosts
+            (@RequestParam(defaultValue = TRUE)
+             @ApiParam(value = IS_PAGINATION_USED, example = TRUE) boolean paged,
+             @RequestParam(defaultValue = ZERO)
+             @ApiParam(value = CURRENT_PAGE_NUMBER, example = ZERO) int page,
+             @RequestParam(defaultValue = FIVE)
+             @ApiParam(value = NUMBER_OF_ITEMS_PER_PAGE, example = FIVE) int size,
+             @RequestParam(defaultValue = ORDER_BY_ID_ASCENDING)
+             @ApiParam(value = SORTING_METHOD, example = ORDER_BY_CREATED_AT_DESCENDING) String sort) {
+        Pageable pageable = paged
+                ? PageRequest.of(page, size, FormatConverter.parseSortString(sort))
+                : Pageable.unpaged();
+
+        List<Post> posts = postService.getPosts(pageable).getContent();
+
+        List<PostsResponseDto> postsResponseDtos = convertPostsToPostsResponseDtos(posts);
+
+        return ResponseEntity.status(HttpStatus.OK).body(postsResponseDtos);
+    }
+
+//    @ApiOperation(value = GET_ALL_POSTS, notes = GET_ALL_POSTS_DESCRIPTION)
+//    @GetMapping()
+//    public ResponseEntity<Page<PostResponseDto>> getAllPosts
+//            (@PathVariable("couponGroupId")
+//             @ApiParam(value = USER_ID_VALUE, example = ID_EXAMPLE) String userId,
+//             @RequestParam(defaultValue = TRUE)
+//             @ApiParam(value = IS_PAGINATION_USED, example = TRUE) boolean paged,
+//             @RequestParam(defaultValue = ZERO)
+//             @ApiParam(value = CURRENT_PAGE_NUMBER, example = ZERO) int page,
+//             @RequestParam(defaultValue = FIVE)
+//             @ApiParam(value = NUMBER_OF_ITEMS_PER_PAGE, example = FIVE) int size,
+//             @RequestParam(defaultValue = ORDER_BY_ID_ASCENDING)
+//             @ApiParam(value = SORTING_METHOD, example = ORDER_BY_ID_DESCENDING) String sort) {
+//        InputFormatValidator.validateId(couponGroupId);
+//        RoleValidator.checkAdminAuthentication();
+//        Pageable pageable = paged ? PageRequest.of(page, size, parseSortString(sort)) : Pageable.unpaged();
+//
+//        Page<Coupon> couponPage = couponGroupService.getCouponGroup(Long.parseLong(couponGroupId), pageable);
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(CouponAndGroupEntityToDtoMapper.toCouponGroupResponseDto(couponPage));
+//    }
 
     @ApiOperation(value = GET_POST, notes = GET_POST_DESCRIPTION)
     @GetMapping("/{postId}")
@@ -75,5 +130,16 @@ public class PostController {
         headers.setLocation(location);
 
         return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(postResponseDto);
+    }
+
+    private List<PostsResponseDto> convertPostsToPostsResponseDtos(List<Post> posts) {
+        List<PostsResponseDto> postsResponseDtos = new ArrayList<>();
+        for (Post post : posts) {
+            View view = viewService.getView(post.getId());
+            PostsResponseDto postsResponseDto = PostsResponseDto.from(post, view.getViewCount());
+            postsResponseDtos.add(postsResponseDto);
+        }
+
+        return postsResponseDtos;
     }
 }
