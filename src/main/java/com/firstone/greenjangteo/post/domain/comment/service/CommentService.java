c36f393ce55020a8f078e849b11e6e75008e8460
Sung Hyobin <hellmir610@gmail.com>
2024-01-09 23:57:53 +0900

    feat(comment): 댓글 목록 조회 기능 프로덕션 코드 추가 (#227)
    - 전체 댓글 목록 조회 요청
    - 전체 댓글 목록 조회 비즈니스 로직
    - 전체 댓글 목록을 페이징 처리하고 생성 순서 내림차순으로 정렬해 조회
    - 200 status code 반환

--- src/main/java/com/firstone/greenjangteo/post/domain/comment/service/CommentService.java
+++ src/main/java/com/firstone/greenjangteo/post/domain/comment/service/CommentService.java
@@ -2,7 +2,11 @@ package com.firstone.greenjangteo.post.domain.comment.service;
 
 import com.firstone.greenjangteo.post.domain.comment.dto.CommentRequestDto;
 import com.firstone.greenjangteo.post.domain.comment.model.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
     Comment createComment(CommentRequestDto commentRequestDto);
+
+    Page<Comment> getComments(Pageable pageable, Long postId);
 }
