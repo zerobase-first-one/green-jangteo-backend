fd14e282eb737a168485b4c4c7f00a9da45976e9
Sung Hyobin <hellmir610@gmail.com>
2024-01-19 20:38:55 +0900

    feat(reserve): 적립금 차감 기능 프로덕션 코드 추가 (#241)
    - 적립금 차감 요청
    - 적립금 차감 요청 비즈니스 로직
    - 204 status code 반환

--- src/main/java/com/firstone/greenjangteo/reserve/controller/ReserveController.java
+++ src/main/java/com/firstone/greenjangteo/reserve/controller/ReserveController.java
@@ -2,6 +2,7 @@ package com.firstone.greenjangteo.reserve.controller;
 
 import com.firstone.greenjangteo.post.dto.PostResponseDto;
 import com.firstone.greenjangteo.reserve.dto.request.AddReserveRequestDto;
+import com.firstone.greenjangteo.reserve.dto.request.UseReserveRequestDto;
 import com.firstone.greenjangteo.reserve.service.ReserveService;
 import com.firstone.greenjangteo.utility.InputFormatValidator;
 import com.firstone.greenjangteo.utility.RoleValidator;
@@ -23,6 +24,9 @@ public class ReserveController {
     private static final String ADD_RESERVE = "적립금 추가";
     private static final String ADD_RESERVE_DESCRIPTION = "회원 ID와 적립 예정 금액을 입력해 적립금을 추가할 수 있습니다.";
 
+    private static final String REDUCE_RESERVE = "적립금 차감";
+    private static final String REDUCE_RESERVE_DESCRIPTION = "회원 ID와 차감 예정 금액을 입력해 적립금을 차감할 수 있습니다.";
+
     @ApiOperation(value = ADD_RESERVE, notes = ADD_RESERVE_DESCRIPTION)
     @PostMapping("/add")
     public ResponseEntity<PostResponseDto> addReserve(@RequestBody AddReserveRequestDto addReserveRequestDto) {
@@ -30,6 +34,17 @@ public class ReserveController {
         RoleValidator.checkAdminAuthentication();
 
         reserveService.addReserve(addReserveRequestDto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @ApiOperation(value = REDUCE_RESERVE, notes = REDUCE_RESERVE_DESCRIPTION)
+    @PostMapping("/reduce")
+    public ResponseEntity<PostResponseDto> reduceReserve(@RequestBody UseReserveRequestDto useReserveRequestDto) {
+        InputFormatValidator.validateId(useReserveRequestDto.getUserId());
+        RoleValidator.checkAdminAuthentication();
+
+        reserveService.reduceReserve(useReserveRequestDto);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
