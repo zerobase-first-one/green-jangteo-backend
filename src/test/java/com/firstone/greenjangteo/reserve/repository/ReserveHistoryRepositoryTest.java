037388808a164346f80d88f3dcf4d6c20148655b
Sung Hyobin <hellmir610@gmail.com>
2024-01-21 23:20:52 +0900

    test(reserve): 적립금 내역 조회 기능 테스트 코드 추가 (#243)
    - 적립금 내역 조회 요청, 200 status code 응답
    - 적립금 내역 조회 비즈니스 로직
    - 적립금 내역을 생성 순서 내림차순으로 정렬해 조회

--- src/test/java/com/firstone/greenjangteo/reserve/repository/ReserveHistoryRepositoryTest.java
+++ src/test/java/com/firstone/greenjangteo/reserve/repository/ReserveHistoryRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,10 +20,12 @@ import static com.firstone.greenjangteo.reserve.testutil.ReserveTestConstant.RES
 import static com.firstone.greenjangteo.reserve.testutil.ReserveTestConstant.RESERVE2;
 import static com.firstone.greenjangteo.user.model.Role.ROLE_BUYER;
 import static com.firstone.greenjangteo.user.testutil.UserTestConstant.*;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 class ReserveHistoryRepositoryTest {
     @Autowired
     private ReserveHistoryRepository reserveHistoryRepository;
@@ -56,5 +59,32 @@ class ReserveHistoryRepositoryTest {
         assertThat(foundReserveHistory.getAddedReserve()).isEqualTo(reserveHistory2.getAddedReserve());
         assertThat(foundReserveHistory.getCurrentReserve()).isEqualTo(reserveHistory2.getCurrentReserve());
         assertThat(foundReserveHistory.getCurrentReserve().getValue()).isEqualTo(2 * RESERVE1 + RESERVE2);
+    }
+
+    @DisplayName("회원 ID를 통해 적립금 내역을 생성시간 오름차순으로 검색할 수 있다.")
+    @Test
+    void findByUserIdOrderByCreatedAtDesc() {
+        // given
+        User user = UserTestObjectFactory.createUser(
+                EMAIL1, USERNAME1, PASSWORD1, passwordEncoder, FULL_NAME1, PHONE1, List.of(ROLE_BUYER.toString())
+        );
+        userRepository.save(user);
+
+        ReserveHistory reserveHistory1
+                = ReserveTestObjectFactory.createReserveHistory(user.getId(), RESERVE1, new CurrentReserve(RESERVE2));
+        ReserveHistory reserveHistory2 = ReserveTestObjectFactory
+                .createReserveHistory(user.getId(), RESERVE1, new CurrentReserve(RESERVE1 + RESERVE2));
+        reserveHistoryRepository.saveAll(List.of(reserveHistory1, reserveHistory2));
+
+        // when
+        List<ReserveHistory> reserveHistories = reserveHistoryRepository.findByUserIdOrderByCreatedAtDesc(user.getId());
+
+        // then
+        assertThat(reserveHistories).hasSize(2)
+                .extracting("addedReserve", "currentReserve")
+                .containsExactly(
+                        tuple(RESERVE1, new CurrentReserve(2 * RESERVE1 + RESERVE2)),
+                        tuple(RESERVE1, new CurrentReserve(RESERVE1 + RESERVE2))
+                );
     }
 }
