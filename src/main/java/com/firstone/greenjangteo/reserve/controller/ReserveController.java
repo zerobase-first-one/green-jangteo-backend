8e25c65fb6ff4e9f992c98cf6b2bdc0d529ccbbe
Hyobin Sung <128391669+hellmir@users.noreply.github.com>
2024-01-28 14:39:13 +0900

    Revert "[#243] 적립금 내역 조회 기능 추가"

--- src/main/java/com/firstone/greenjangteo/reserve/controller/ReserveController.java
+++ src/main/java/com/firstone/greenjangteo/reserve/controller/ReserveController.java
@@ -3,23 +3,17 @@ package com.firstone.greenjangteo.reserve.controller;
 import com.firstone.greenjangteo.post.dto.PostResponseDto;
 import com.firstone.greenjangteo.reserve.dto.request.AddReserveRequestDto;
 import com.firstone.greenjangteo.reserve.dto.request.UseReserveRequestDto;
-import com.firstone.greenjangteo.reserve.dto.response.ReserveResponseDto;
-import com.firstone.greenjangteo.reserve.model.entity.ReserveHistory;
 import com.firstone.greenjangteo.reserve.service.ReserveService;
 import com.firstone.greenjangteo.utility.InputFormatValidator;
 import com.firstone.greenjangteo.utility.RoleValidator;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.firstone.greenjangteo.web.ApiConstant.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reserves")
@@ -32,11 +26,6 @@ public class ReserveController {
 
     private static final String REDUCE_RESERVE = "적립금 차감";
     private static final String REDUCE_RESERVE_DESCRIPTION = "회원 ID와 차감 예정 금액을 입력해 적립금을 차감할 수 있습니다.";
-
-    private static final String GET_RESERVE_HISTORIES = "적립금 히스토리 조회";
-    private static final String GET_RESERVE_HISTORIES_DESCRIPTION
-            = "회원 ID를 입력해 적립금의 적립, 사용 내역을 조회할 수 있습니다.";
-
 
     @ApiOperation(value = ADD_RESERVE, notes = ADD_RESERVE_DESCRIPTION)
     @PostMapping("/add")
@@ -58,19 +47,5 @@ public class ReserveController {
         reserveService.reduceReserve(useReserveRequestDto);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @ApiOperation(value = GET_RESERVE_HISTORIES, notes = GET_RESERVE_HISTORIES_DESCRIPTION)
-    @PreAuthorize(PRINCIPAL_POINTCUT)
-    @GetMapping()
-    public ResponseEntity<List<ReserveResponseDto>> getReserveHistories
-            (@RequestParam(name = "userId") @ApiParam(value = USER_ID_VALUE, example = ID_EXAMPLE) String userId) {
-        InputFormatValidator.validateId(userId);
-
-        List<ReserveHistory> reserveHistories = reserveService.getReserveHistories(Long.parseLong(userId));
-        List<ReserveResponseDto> reserveResponseDtos
-                = reserveHistories.stream().map(ReserveResponseDto::from).collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.OK).body(reserveResponseDtos);
     }
 }
--- src/main/java/com/firstone/greenjangteo/reserve/repository/ReserveHistoryRepository.java
+++ src/main/java/com/firstone/greenjangteo/reserve/repository/ReserveHistoryRepository.java
@@ -3,11 +3,8 @@ package com.firstone.greenjangteo.reserve.repository;
 import com.firstone.greenjangteo.reserve.model.entity.ReserveHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ReserveHistoryRepository extends JpaRepository<ReserveHistory, Long> {
     Optional<ReserveHistory> findFirstByUserIdOrderByCreatedAtDesc(Long userId);
-
-    List<ReserveHistory> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
